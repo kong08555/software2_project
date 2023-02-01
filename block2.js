2eb31a8e859a063cc7724cc04276c07d57c2dd6a
kong <kong08555@gmail.com>
2023-02-01 11:14:31 +0700

    ss

--- block2.js
+++ block2.js
@@ -7,7 +7,7 @@ var f_name = document.getElementById("f_name");
           var dataList = JSON.parse(localStorage.getItem("arr_empty"));
           var count_i = JSON.parse(localStorage.getItem("i"));
           console.log("create empty array: ", count_i ,dataList);
-          console.log(localStorage.getItem("count"));
+          console.log(localStorage.getItem("i"));
 
           sub_list.addEventListener("click",function(){
             dataList.push("name: "+f_name.value +" "+ l_name.value + "<br> faculty: "+ faculty.value + "<br> personal ID: "+ p_id.value);
--- homeProject.js
+++ homeProject.js
@@ -5,8 +5,9 @@ function create_arr(){
     for(let i = 0;i > button;i++){
     arr_empty = [];
     count++;
+    var key_i = localStorage.key(i);
     localStorage.setItem("button",button);
-    localStorage.setItem("arr_empty",JSON.stringify(arr_empty));
-    localStorage.setItem(`item${i}`, `This is item number ${i}`);
+    localStorage.setItem("arr_empty",JSON.stringify(arr_empty))
+    
 }
 }
