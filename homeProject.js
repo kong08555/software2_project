ba39a227cdfd8bf31703c275a99a0fbea16bf4a3
kong <kong08555@gmail.com>
2023-02-01 11:31:39 +0700

    c

--- homeProject.js
+++ homeProject.js
@@ -2,9 +2,9 @@ let arr_empty;
 let count = 0;
 let button = document.getElementById("btn-2");
 function create_arr(){
-    for(let i = 0;i > button;i++){
-    arr_empty = [];
     count++;
+    for(let i = 0;i > count;i++){
+    arr_empty = [];
     var key_i = localStorage.key(i);
     localStorage.setItem("button",button);
     localStorage.setItem("arr_empty",JSON.stringify(arr_empty))
